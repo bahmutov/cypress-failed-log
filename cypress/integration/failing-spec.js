6e97354b0d3c149a5360575a4b3744582d80078c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-29 22:32:11 -0500

    afterEach test will navigate to another page and run dummy commands

--- cypress/integration/failing-spec.js
+++ cypress/integration/failing-spec.js
@@ -1,6 +1,22 @@
 describe('cypress failed log', () => {
+  const url = 'https://glebbahmutov.com'
+
   beforeEach(() => {
-    cy.visit('https://glebbahmutov.com')
+    cy.visit(url)
+  })
+
+  afterEach(() => {
+    // more dummy commands on purpose. Can we get
+    // the right screenshot when the test actuall failed?
+    cy.visit(url)
+      .wait(100)
+      .wait(100)
+      .wait(100)
+      .wait(100)
+      .wait(100)
+      .wait(100)
+      .wait(100)
+      .wait(100)
   })
 
   // this test fails on purpose
