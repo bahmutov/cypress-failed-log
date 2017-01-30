1d04ab7b28a83079034feb3ee32ec007afc450e7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-30 11:15:28 -0500

    move verify json to test folder

---
+++ test/verify-failed-json.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const la = require('lazy-ass')
+const is = require('check-more-types')
+const fs = require('fs')
+const path = require('path')
+
+const jsonFiles = [
+  'failed-cypress-failed-log-finds-aliens.json'
+]
+
+function checkJsonFile (filename) {
+  la(is.unemptyString(filename), 'expected filename', filename)
+  const jsonFilename = path.join(__dirname, '..', filename)
+  la(fs.existsSync(jsonFilename), 'cannot find json file', jsonFilename)
+
+  const result = require(jsonFilename)
+  la(is.object(result), 'expected an object from', jsonFilename, result)
+
+  la(is.unemptyString(result.title), 'missing test title', result)
+  la(is.unemptyString(result.testError), 'missing test error', result)
+  la(is.strings(result.testCommands), 'missing test commands', result)
+  la(result.testCommands[0].startsWith('visit'),
+    'expected first command to be visit', result.testCommands)
+
+  console.log('file %s looks ok', jsonFilename)
+}
+
+jsonFiles.forEach(checkJsonFile)
