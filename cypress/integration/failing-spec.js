7987ad9bbebd0efe0bc85db0c19e309db189d138
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-13 13:42:48 -0500

    update the test a little

--- cypress/integration/failing-spec.js
+++ cypress/integration/failing-spec.js
@@ -6,8 +6,7 @@ describe('cypress failed log', () => {
   afterEach(function makeDummyCommands () {
     // more dummy commands on purpose. Can we get
     // the right screenshot when the test actual failed?
-    cy.visit('/')
-      .wait(100)
+    cy.wait(100)
       .wait(100)
       .wait(100)
       .wait(100)
@@ -19,14 +18,8 @@ describe('cypress failed log', () => {
 
   // this test fails on purpose
   it('finds aliens', () => {
-    cy.contains('a', 'videos')
-      .click()
-      .wait(100)
-
-    cy.url()
-      .should('contain', 'videos')
-
-    cy.contains('Aliens')
+    cy.log('fail on purpose, no such text')
+    cy.contains('this text does not exist')
       .should('be.visible')
   })
 })
