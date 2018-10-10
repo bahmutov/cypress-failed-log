c6d83efbb684847e40857e16f3f10a1c43a0327d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-10-10 10:49:37 -0400

    chore: check if log files exist

---
+++ __snapshots__/cypress-failed-log-spec.js
@@ -0,0 +1,7 @@
+exports['spec a.js finished with'] = {
+  "totalTests": 3,
+  "totalFailed": 2,
+  "totalPassed": 1,
+  "totalPending": 0,
+  "totalSkipped": 0
+}
--- src/cypress-failed-log-spec.js
+++ src/cypress-failed-log-spec.js
@@ -1,7 +1,50 @@
 'use strict'
 
+const la = require('lazy-ass')
+const cypress = require('cypress')
+const snapshot = require('snap-shot-it')
+const { pick } = require('ramda')
+const { existsSync } = require('fs')
+const { join } = require('path')
+
 /* global describe, it */
-describe('empty test suite', () => {
-  it('write this test', () => {
+describe('cypress-failed-log', () => {
+  it('runs spec a', () => {
+    return cypress
+      .run({
+        spec: 'cypress/integration/a.js'
+      })
+      .then(
+        pick([
+          'totalTests',
+          'totalFailed',
+          'totalPassed',
+          'totalPending',
+          'totalSkipped'
+        ])
+      )
+      .then(tests => {
+        snapshot('spec a.js finished with', tests)
+      })
+      .then(() => {
+        const logFilename = join(
+          __dirname,
+          '..',
+          'cypress',
+          'logs',
+          'failed-root-suite-first-context-has-second-test-failing.json'
+        )
+        la(existsSync(logFilename), 'cannot find file', logFilename)
+      })
+      .then(() => {
+        const logFilename = join(
+          __dirname,
+          '..',
+          'cypress',
+          'logs',
+          'failed-root-suite-first-context-has-third-test-failing.json'
+        )
+        la(existsSync(logFilename), 'cannot find file', logFilename)
+      })
   })
 })
