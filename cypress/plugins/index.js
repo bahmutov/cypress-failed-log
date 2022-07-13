af9c7e041307c4c6a880a497df00824dc965a95e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-07-13 10:05:40 -0400

    update the config file to use on to register

--- cypress/plugins/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (on, config) => {
-  on('task', {
-    // in the user project this would be
-    // failed: require('cypress-failed-log/src/failed')()
-    failed: require('../../src/failed')()
-  })
-}
